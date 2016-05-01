e59abff00ddca5ac3a3c1d3bab35c41ea1085933
Pavol Pidanič <paulp@hotmail.sk>
2016-05-01 17:06:38 +0200

    Create BrokenGreetings.java

---
+++ broken_greetings/BrokenGreetings.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+
+    public Person(String personName) {
+        name = personName;
+    }
+
+    public String greet(String yourName) {
+        return String.format("Hi %s, my name is %s", yourName, name);
+    }
+}
